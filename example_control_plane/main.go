22191e66537a74c45ec2dba2f26836349002b544
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-10-01 13:14:53 -0400

    add logging

--- example_control_plane/main.go
+++ example_control_plane/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/danthegoodman1/Gildra/acme_http"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/samber/lo"
 	"log"
 	"net/http"
@@ -34,6 +35,17 @@ func main() {
 	server.HTTPErrorHandler = customHTTPErrorHandler
 	log.Println("starting example control plane on :8080")
 
+	logConfig := middleware.LoggerConfig{
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}",` +
+			`"host":"${host}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
+			`"status":${status},"error":"${error}","latency":${latency},"latency_human":"${latency_human}",` +
+			`"bytes_in":${bytes_in},"bytes_out":${bytes_out},"proto":"${protocol}",` +
+			// fake request headers for getting info into http logs
+			`"userID":"${header:loguserid}","reqID":"${header:reqID}","wasCached":"${header:wasCached}"}` + "\n",
+		CustomTimeFormat: "2006-01-02 15:04:05.00000",
+		Output:           os.Stdout, // logger or os.Stdout
+	}
+	server.Use(middleware.LoggerWithConfig(logConfig))
 	server.GET("/echo", echoHandler)
 	server.POST("/create_cert", createCert)
 	server.GET("/domains/:domain/token/:key", getToken)
