b2e28b65bd46613b7eea72a68714babc9e3a6136
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-12-24 10:04:15 -0500

    fix logger

--- http_server/acme_challenge.go
+++ http_server/acme_challenge.go
@@ -18,50 +18,50 @@ func handleHTTPChallenge(ctx context.Context, fqdn string, w http.ResponseWriter
 
 	if strings.HasPrefix(r.URL.Path, ZeroSSLPathPrefix) {
 		span.SetAttributes(attribute.Bool("zeroSSLChallenge", true))
-		logger.Debug().Msgf("got ZeroSSL HTTP challenge request for FQDN %s", fqdn)
+		globalLogger.Debug().Msgf("got ZeroSSL HTTP challenge request for FQDN %s", fqdn)
 
 		_, token := path.Split(r.URL.Path)
-		logger.Debug().Msg(fmt.Sprint("Got challenge for fqdn", fqdn, "token", token))
+		globalLogger.Debug().Msg(fmt.Sprint("Got challenge for fqdn", fqdn, "token", token))
 		key, err := control_plane.GetHTTPChallengeKey(fqdn, token)
 		if err != nil {
-			logger.Error().Err(err).Msgf("error in GetHTTPChallengeToken for FQDN %s", fqdn)
+			globalLogger.Error().Err(err).Msgf("error in GetHTTPChallengeToken for FQDN %s", fqdn)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		logger.Debug().Msg(fmt.Sprint("Got key", key))
+		globalLogger.Debug().Msg(fmt.Sprint("Got key", key))
 		w.WriteHeader(http.StatusOK)
 		_, err = w.Write([]byte(key))
 		if err != nil {
-			logger.Error().Err(err).Msg("error in writing bytes to response for HTTP ZeroSSL challenge")
+			globalLogger.Error().Err(err).Msg("error in writing bytes to response for HTTP ZeroSSL challenge")
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		logger.Debug().Msg(fmt.Sprint("wrote response", key))
+		globalLogger.Debug().Msg(fmt.Sprint("wrote response", key))
 		internal.Metric_ZEROSSL_HTTP_Challenges.Inc()
 		return
 	}
 
 	// Otherwise ACME
 	span.SetAttributes(attribute.Bool("acmeChallenge", true))
-	logger.Debug().Msgf("got ACME HTTP challenge request for FQDN %s", fqdn)
+	globalLogger.Debug().Msgf("got ACME HTTP challenge request for FQDN %s", fqdn)
 
 	_, token := path.Split(r.URL.Path)
-	logger.Debug().Msg(fmt.Sprint("Got challenge for fqdn", fqdn, "token", token))
+	globalLogger.Debug().Msg(fmt.Sprint("Got challenge for fqdn", fqdn, "token", token))
 	key, err := control_plane.GetHTTPChallengeKey(fqdn, token)
 	if err != nil {
-		logger.Error().Err(err).Msgf("error in GetHTTPChallengeToken for FQDN %s", fqdn)
+		globalLogger.Error().Err(err).Msgf("error in GetHTTPChallengeToken for FQDN %s", fqdn)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	logger.Debug().Msg(fmt.Sprint("Got key", key))
+	globalLogger.Debug().Msg(fmt.Sprint("Got key", key))
 	w.WriteHeader(http.StatusOK)
 	_, err = w.Write([]byte(key))
 	if err != nil {
-		logger.Error().Err(err).Msg("error in writing bytes to response for HTTP ACME challenge")
+		globalLogger.Error().Err(err).Msg("error in writing bytes to response for HTTP ACME challenge")
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	logger.Debug().Msg(fmt.Sprint("wrote response", key))
+	globalLogger.Debug().Msg(fmt.Sprint("wrote response", key))
 	internal.Metric_ACME_HTTP_Challenges.Inc()
 	return
 }
